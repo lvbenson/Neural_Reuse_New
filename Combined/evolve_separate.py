4cfb24201ced2d763be563e0f5b9c3f28442b5f0
Lauren Benson <lvbenson@iu.edu>
2020-10-22 12:17:48 -0400

    legged walker changes

--- Combined/evolve_separate.py
+++ Combined/evolve_separate.py
@@ -4,12 +4,12 @@ import invpend              #Task 1
 import cartpole             #Task 2
 import leggedwalker         #Task 3
 import mountaincar          #Task 4
-
+import os
 import numpy as np
 import sys
 
 #id = str(sys.argv[1])
-id = 'multi'
+id = 'lw_new'
 
 # ANN Params
 #nI = 3+4+3 #+2
@@ -27,7 +27,7 @@ genesize = (nI*nH1) + (nH1*nH2) + (nH1*nO) + nH1 + nH2 + nO # 115 parameters
 recombProb = 0.5
 mutatProb = 0.05 # 1/genesize # 1/g = 0.0086 we can make it 0.01 because with 1/g, avg seemed to trail close to best.
 demeSize = 49
-generations = 100 #1000 # With 150 (17hours), lots of progress, but more possible easily (with 300, 34hours);
+generations = 50 #1000 # With 150 (17hours), lots of progress, but more possible easily (with 300, 34hours);
 boundaries = 0
 
 # Task Params
@@ -67,11 +67,11 @@ x_range_CP = np.linspace(0.0, 0.0, num=trials_x_CP) #x_range_CP = np.linspace(-0
 xdot_range_CP = np.linspace(0.0, 0.0, num=trials_xdot_CP) #xdot_range_CP = np.linspace(-0.05, 0.05, num=trials_xdot_CP)
 
 #Legged walker ## 9 * 2200 = 19,800 updates ==> 4 * 1100 = 4400
-trials_theta = 1
-theta_range_LW = np.linspace(-np.pi/6, np.pi/6, num=trials_theta)
+trials_theta_LW = 1
+theta_range_LW = np.linspace(-np.pi/6, np.pi/6, num=trials_theta_LW)
 trials_omega_LW = 1
 omega_range_LW = np.linspace(-1.0, 1.0, num=trials_omega_LW)
-total_trials_LW = trials_theta * trials_omega_LW
+total_trials_LW = trials_theta_LW * trials_omega_LW
 
 #Mountain Car
 trials_position_MC = 3 #6
@@ -79,9 +79,9 @@ trials_velocity_MC = 3 #6
 total_trials_MC = trials_position_MC*trials_velocity_MC
 position_range_MC = np.linspace(0.1, 0.1, num=trials_position_MC)
 velocity_range_MC = np.linspace(0.01,0.01, num=trials_velocity_MC)
-
+"""
 # Fitness function
-def fitnessFunction(genotype):
+def fitnessFunctionInv(genotype):
     nn = ffann.ANN(nI,nH1,nH2,nO)
     nn.setParameters(genotype,WeightRange,BiasRange)
     # Task 1
@@ -101,6 +101,153 @@ def fitnessFunction(genotype):
     fitness1 = (fitness1+7.65)/7 # Normalize to run between 0 and 1
     if fitness1 < 0.0:
         fitness1 = 0.0
+    #return fitness1
+
+ga = mga.Microbial(fitnessFunctionInv, popsize, genesize, recombProb, mutatProb, demeSize, generations, boundaries)
+ga.run()
+ga.showFitness()
+
+# Get best evolved network and show its activity
+af,bf,bi = ga.fitStats()
+
+np.save('average_history_'+id+'.npy',ga.avgHistory)
+np.save('best_history_'+id+'.npy',ga.bestHistory)
+np.save('best_individual_'+id+'.npy',bi)
+
+
+def fitnessFunctionCP(genotype):
+    nn = ffann.ANN(nI,nH1,nH2,nO)
+    nn.setParameters(genotype,WeightRange,BiasRange)
+    # Task 2
+    body = cartpole.Cartpole()
+    fit = 0.0
+    for theta in theta_range_CP:
+        for theta_dot in thetadot_range_CP:
+            for x in x_range_CP:
+                for x_dot in xdot_range_CP:
+                    body.theta = theta
+                    body.theta_dot = theta_dot
+                    body.x = x
+                    body.x_dot = x_dot
+                    for t in time_CP:
+
+                        nn.step(body.state())
+
+                        f,done = body.step(stepsize_CP, np.array([nn.output()[0]]))
+                        fit += f  # (Maximize) Amount of time pole is balanced
+                        ###
+                        if done:
+                            break
+    fitness2 = fit/(duration_CP*total_trials_CP)
+    if fitness2 < 0.0:
+        fitness2 = 0.0
+    return fitness2
+ga = mga.Microbial(fitnessFunctionCP, popsize, genesize, recombProb, mutatProb, demeSize, generations, boundaries)
+ga.run()
+ga.showFitness()
+
+# Get best evolved network and show its activity
+af,bf,bi = ga.fitStats()
+
+np.save('average_history_'+id+'.npy',ga.avgHistory)
+np.save('best_history_'+id+'.npy',ga.bestHistory)
+np.save('best_individual_'+id+'.npy',bi)
+
+"""
+def fitnessFunctionLW(genotype):
+    nn = ffann.ANN(nI,nH1,nH2,nO)
+    nn.setParameters(genotype,WeightRange,BiasRange)
+    #Task 3
+    body = leggedwalker.LeggedAgent(0.0,0.0)
+    fit = 0.0
+    for theta in theta_range_LW:
+        for omega in omega_range_LW:
+            body.reset()
+            body.angle = theta
+            body.omega = omega
+            for t in time_LW:
+
+                #nn.step(np.concatenate((body.state(),np.zeros(1))))
+                #nn.step(np.concatenate((body.state(),np.zeros(1))))
+                nn.step(body.state())
+
+                body.step(stepsize_LW, np.array([nn.output()[0]]))
+            fit += body.cx/duration_LW # Maximize the final forward distance covered
+    fitness3 = (fit/total_trials_LW)/MaxFit
+    if fitness3 < 0.0:
+        fitness3 = 0.0
+    return fitness3
+
+ga = mga.Microbial(fitnessFunctionLW, popsize, genesize, recombProb, mutatProb, demeSize, generations, boundaries)
+ga.run()
+ga.showFitness()
+
+# Get best evolved network and show its activity
+af,bf,bi = ga.fitStats()
+
+np.save('average_history_'+id+'.npy',ga.avgHistory)
+np.save('best_history_'+id+'.npy',ga.bestHistory)
+np.save('best_individual_'+id+'.npy',bi)
+
+
+"""
+def fitnessFunctionMC(genotype):
+    nn = ffann.ANN(nI,nH1,nH2,nO)
+    nn.setParameters(genotype,WeightRange,BiasRange)
+    # # Task 4
+    body = mountaincar.MountainCar()
+    fit = 0.0
+    for position in position_range_MC:
+        for velocity in velocity_range_MC:
+            body.position = position
+            body.velocity = velocity
+            for t in time_MC:
+                nn.step(np.concatenate((body.state(),np.zeros(2))))
+                
+                f,done = body.step(stepsize_MC, np.array([nn.output()[0]]))
+                fit += f
+                if done:
+                    break
+    fitness4 = ((fit/(duration_MC*total_trials_MC)) + 1.0)/0.65
+    return fitness4
+    #return fitness1*fitness2*fitness3
+
+# Evolve and visualize fitness over generations
+ga = mga.Microbial(fitnessFunctionMC, popsize, genesize, recombProb, mutatProb, demeSize, generations, boundaries)
+ga.run()
+ga.showFitness()
+
+# Get best evolved network and show its activity
+af,bf,bi = ga.fitStats()
+
+np.save('average_history_'+id+'.npy',ga.avgHistory)
+np.save('best_history_'+id+'.npy',ga.bestHistory)
+np.save('best_individual_'+id+'.npy',bi)
+
+
+
+# Fitness function
+def fitnessFunctionIVCP(genotype):
+    nn = ffann.ANN(nI,nH1,nH2,nO)
+    nn.setParameters(genotype,WeightRange,BiasRange)
+    # Task 1
+    body = invpend.InvPendulum()
+    fit = 0.0
+    for theta in theta_range_IP:
+        for theta_dot in thetadot_range_IP:
+            body.theta = theta
+            body.theta_dot = theta_dot
+            for t in time_IP:
+
+                nn.step(np.concatenate((body.state(),np.zeros(1))))
+
+                f = body.step(stepsize_IP, np.array([nn.output()[0]]))
+                fit += f    # Minimize the cost of moving the pole up
+    fitness1 = fit/(duration_IP*total_trials_IP)
+    fitness1 = (fitness1+7.65)/7 # Normalize to run between 0 and 1
+    if fitness1 < 0.0:
+        fitness1 = 0.0
+    #return fitness1
 
     # Task 2
     body = cartpole.Cartpole()
@@ -125,45 +272,8 @@ def fitnessFunction(genotype):
     fitness2 = fit/(duration_CP*total_trials_CP)
     if fitness2 < 0.0:
         fitness2 = 0.0
-
-    #Task 3
-    body = leggedwalker.LeggedAgent(0.0,0.0)
-    fit = 0.0
-    for theta in theta_range_LW:
-        for omega in omega_range_LW:
-            body.reset()
-            body.angle = theta
-            body.omega = omega
-            for t in time_LW:
-
-                nn.step(body.state())
-                #nn.step(np.concatenate((body.state(),np.zeros(1))))))
-                body.step(stepsize_LW, np.array([nn.output()[0]]))
-            fit += body.cx/duration_LW # Maximize the final forward distance covered
-    fitness3 = (fit/total_trials_LW)/MaxFit
-    if fitness3 < 0.0:
-        fitness3 = 0.0
-
-    # # Task 4
-    body = mountaincar.MountainCar()
-    fit = 0.0
-    for position in position_range_MC:
-        for velocity in velocity_range_MC:
-            body.position = position
-            body.velocity = velocity
-            for t in time_MC:
-                nn.step(np.concatenate((body.state(),np.zeros(2))))
-                
-                f,done = body.step(stepsize_MC, np.array([nn.output()[0]]))
-                fit += f
-                if done:
-                    break
-    fitness4 = ((fit/(duration_MC*total_trials_MC)) + 1.0)/0.65
-    return fitness1*fitness2*fitness3*fitness4
-    #return fitness1*fitness2*fitness3
-
-# Evolve and visualize fitness over generations
-ga = mga.Microbial(fitnessFunction, popsize, genesize, recombProb, mutatProb, demeSize, generations, boundaries)
+    return fitness1*fitness2
+ga = mga.Microbial(fitnessFunctionIVCP, popsize, genesize, recombProb, mutatProb, demeSize, generations, boundaries)
 ga.run()
 ga.showFitness()
 
@@ -173,3 +283,4 @@ af,bf,bi = ga.fitStats()
 np.save('average_history_'+id+'.npy',ga.avgHistory)
 np.save('best_history_'+id+'.npy',ga.bestHistory)
 np.save('best_individual_'+id+'.npy',bi)
+"""
--- Combined/leggedwalker.py
+++ Combined/leggedwalker.py
@@ -1,13 +1,6 @@
 import math
 import numpy as np
 
-# Nervous system
-BiasRange = 16                # Range of bias [
-WeightRange = 16              # Range of weights [-16, 16]
-SensorWeightRange = 16
-TimeConstMax = 1        # Maximum time constant parameter
-TimeConstMin = 0.1
-
 # Constants
 LegLength = 15
 MaxLegForce = 0.05
@@ -16,11 +9,6 @@ BackwardAngleLimit = -math.pi/6.0
 MaxVelocity = 6.0
 MaxTorque = 0.5
 MaxOmega = 1.0
-
-# Neuron roles
-FS = 0
-FM = 1
-BM = 2
 
 class LeggedAgent:
 
@@ -54,12 +42,66 @@ class LeggedAgent:
         self.footX = self.jointX + LegLength * math.sin(self.angle)
         self.footY = self.jointY + LegLength * math.cos(self.angle)
 
-    def state(self):
-        #return np.array([self.angle, self.omega, self.footstate])
-        #theta, theta_dot, footstate
-        return np.array([self.angle, self.omega, self.footstate])
-
     def step(self, stepsize, u):
+
+        u = (u+1)/2.0
+        force = 0.0
+
+        # Update the leg effectors
+        if (u[0] > 0.5):
+            self.footstate = 1
+            self.omega = 0
+            self.forwardForce = 2 * (u[0] - 0.5) * MaxLegForce
+            self.backwardForce = 0.0
+        else:
+            self.footstate = 0
+            self.forwardForce = 0.0
+            self.backwardForce = 2 * (0.5 - u[0]) * MaxLegForce
+
+        # Compute force applied to the body
+        f = self.forwardForce - self.backwardForce
+        if (self.footstate == 1.0):
+            if ((self.angle >= BackwardAngleLimit and self.angle <= ForwardAngleLimit) or
+                (self.angle < BackwardAngleLimit and f < 0) or
+                (self.angle > ForwardAngleLimit and f > 0)):
+                force = f
+
+        # Update the position of the body
+        self.vx = self.vx + stepsize * force
+        if (self.vx < -MaxVelocity):
+            self.vx = -MaxVelocity
+        if (self.vx > MaxVelocity):
+            self.vx = MaxVelocity
+        self.cx = self.cx + stepsize * self.vx
+
+        # Update the leg geometry
+        self.jointX = self.jointX + stepsize * self.vx
+        if (self.footstate == 1.0):
+            angle = math.atan2(self.footX - self.jointX, self.footY - self.jointY)
+            self.omega = (angle - self.angle)/stepsize
+            self.angle = angle
+        else:
+            self.vx = 0.0
+            self.omega = self.omega + stepsize * MaxTorque * (self.backwardForce - self.forwardForce)
+            if (self.omega < -MaxOmega):
+                self.omega = -MaxOmega
+            if (self.omega > MaxOmega):
+                self.omega = MaxOmega
+            self.angle = self.angle + stepsize * self.omega
+            if (self.angle < BackwardAngleLimit):
+                self.angle = BackwardAngleLimit
+                self.omega = 0
+            if (self.angle > ForwardAngleLimit):
+                self.angle = ForwardAngleLimit
+                self.omega = 0
+            self.footX = self.jointX + LegLength * math.sin(self.angle)
+            self.footY = self.jointY + LegLength * math.cos(self.angle)
+
+        # If the foot is too far back, the body becomes "unstable" and forward motion ceases
+        if (self.cx - self.footX > 20):
+            self.vx = 0.0
+
+    def step3(self, stepsize, u):
 
         u = (u+1)/2.0
         force = 0.0
@@ -114,3 +156,6 @@ class LeggedAgent:
         # If the foot is too far back, the body becomes "unstable" and forward motion ceases
         if (self.cx - self.footX > 20):
             self.vx = 0.0
+
+    def state(self):
+        return np.array([self.angle, self.omega, self.footstate, 0.0])
