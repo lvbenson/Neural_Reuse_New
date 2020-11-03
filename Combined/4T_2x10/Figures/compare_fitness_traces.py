da4028703a7676215fefc98be3c60fad7a840761
Benson <lvbenson@iu.edu>
2020-11-03 16:11:39 -0500

    new analysis

--- Combined/4T_2x10/Figures/compare_fitness_traces.py
+++ Combined/4T_2x10/Figures/compare_fitness_traces.py
@@ -1,9 +1,10 @@
 import os
 import glob
+import pandas as pd
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+"""
 def fitness_traces(dir, color, label):
     files = glob.glob(os.path.join(dir, "best_history*.npy"))
     print("Found {} files in {}".format(len(files), dir))
@@ -12,6 +13,7 @@ def fitness_traces(dir, color, label):
         bf = np.load(file)
         bf = bf**(1/4)
         bfs[i] = bf[-1]
+        print(bfs[i])
         if dir == "./Combined/4T_2x10/Data":
             plt.plot(bf, color=color, label=label)
         else:
@@ -28,7 +30,7 @@ plt.figure(figsize=[4, 3])
 #fitness_traces("./Combined/4T_2x10/Data", "xkcd:green", "2x5")
 fitness_traces("./Combined/4T_2x5/Data", "xkcd:azure", "2x10")
 fitness_traces("./Combined/4T_2x10/Data", "xkcd:green", "2x5")
-fitness_traces("./Combined/4T_2x20/Data", "xkcd:red", "2x20")
+#fitness_traces("./Combined/4T_2x20/Data", "xkcd:red", "2x20")
 
 plt.legend()
 plt.xlabel("Generations")
@@ -36,3 +38,52 @@ plt.ylabel("Fitness")
 plt.title("fitness traces")
 plt.savefig("./Combined/4T_2x10/Figures/figure_2_compare.pdf")
 plt.show()
+"""
+
+
+####Distribution plot
+
+def distributions(dir1, dir2, dir3):
+    files1 = glob.glob(os.path.join(dir1, "best_history*.npy"))
+    print("Found {} files in {}".format(len(files1), dir1))
+    files2 = glob.glob(os.path.join(dir2, "best_history*.npy"))
+    print("Found {} files in {}".format(len(files2), dir2))
+    files3 = glob.glob(os.path.join(dir3, "best_history*.npy"))
+    print("Found {} files in {}".format(len(files3), dir3))
+    bfs1 = np.zeros(len(files1))
+    fits1_list = []
+    for i, file in enumerate(files1):
+        bf1 = np.load(file)
+        bf1 = bf1**(1/4)
+        bfs1[i] = bf1[-1]
+        fits1_list.append(bfs1[i])
+    bfs2 = np.zeros(len(files1))
+    fits2_list = []
+    for i, file in enumerate(files2):
+        bf2 = np.load(file)
+        bf2 = bf2**(1/4)
+        bfs2[i] = bf2[-1]
+        fits2_list.append(bfs2[i])
+    bfs3 = np.zeros(len(files3))
+    fits3_list = []
+    for i, file in enumerate(files3):
+        bf3 = np.load(file)
+        bf3 = bf3**(1/4)
+        bfs3[i] = bf3[-1]
+        fits3_list.append(bfs3[i])
+    return fits1_list,fits2_list,fits3_list
+
+exp1,exp2,exp3 = distributions("./Combined/4T_2x5/Data", "./Combined/4T_2x10/Data","./Combined/4T_2x20/Data")
+
+colors = ['#E69F00', '#56B4E9','#009E73']
+names = ['2x5','2x10','2x20']
+
+plt.hist([exp1,exp2,exp3], bins = int(1/.1), color = colors, label=names)
+
+plt.legend()
+plt.xlabel('fitness')
+plt.ylabel('# of agents')
+plt.title('Fitness distributions')
+plt.savefig("./Combined/4T_2x10/Figures/figure_2_distributions.pdf")
+plt.show()
+    
--- Combined/4T_2x5/scripts/analysis.py
+++ Combined/4T_2x5/scripts/analysis.py
@@ -42,17 +42,17 @@ MaxFit = 0.627 #Leggedwalker
 
 # Fitness initialization ranges
 #Inverted Pendulum
-trials_theta_IP = 3
-trials_thetadot_IP = 3
+trials_theta_IP = 10
+trials_thetadot_IP = 10
 total_trials_IP = trials_theta_IP*trials_thetadot_IP
 theta_range_IP = np.linspace(-np.pi, np.pi, num=trials_theta_IP)
 thetadot_range_IP = np.linspace(-1.0,1.0, num=trials_thetadot_IP)
 
 #Cartpole
-trials_theta_CP = 3
-trials_thetadot_CP = 3
-trials_x_CP = 1
-trials_xdot_CP = 1
+trials_theta_CP = 10
+trials_thetadot_CP = 10
+trials_x_CP = 2
+trials_xdot_CP = 2
 total_trials_CP = trials_theta_CP*trials_thetadot_CP*trials_x_CP*trials_xdot_CP
 theta_range_CP = np.linspace(-0.05, 0.05, num=trials_theta_CP)
 thetadot_range_CP = np.linspace(-0.05, 0.05, num=trials_thetadot_CP)
@@ -60,15 +60,15 @@ x_range_CP = np.linspace(0.0, 0.0, num=trials_x_CP)
 xdot_range_CP = np.linspace(0.0, 0.0, num=trials_xdot_CP)
 
 #Legged walker
-trials_theta = 1
+trials_theta = 10
 theta_range_LW = np.linspace(0.0, 0.0, num=trials_theta)
-trials_omega_LW = 1
+trials_omega_LW = 10
 omega_range_LW = np.linspace(0.0, 0.0, num=trials_omega_LW)
 total_trials_LW = trials_theta * trials_omega_LW
 
 #Mountain Car
-trials_position_MC = 3 #6
-trials_velocity_MC = 3 #6
+trials_position_MC = 6 #6
+trials_velocity_MC = 6 #6
 total_trials_MC = trials_position_MC*trials_velocity_MC
 position_range_MC = np.linspace(0.1, 0.1, num=trials_position_MC)
 velocity_range_MC = np.linspace(0.01,0.01, num=trials_velocity_MC)
@@ -534,7 +534,7 @@ for i in range(start,finish):
         #print('perf:',np.prod(f)**(1/4))
         #print('evol:',bf[index][-1]**(1/4))
         #plt.scatter(np.arange(1, 11), f, c='red',s=5, alpha=0.7)
-        #plt.scatter(np.prod(f)**(1/4), bf[index][-1]**(1/4), c='blue',s=5, alpha=0.7)
+        plt.scatter(np.prod(f)**(1/4), bf[index][-1]**(1/4), c='blue',s=5, alpha=0.7)
         
         
         np.save(dir+"/perf_"+str(i)+".npy",f)
@@ -589,9 +589,10 @@ for i in range(start,finish):
     #plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     #plt.tight_layout()
     #plt.savefig("./Combined/Experiments/Comb_4T_2x5_NEW/Figures/figure_5_lesions.pdf")
-#plt.xlabel("performance")
-#plt.ylabel("evolved fitness")
-#plt.show()
+plt.xlabel("performance")
+plt.ylabel("evolved fitness")
+plt.savefig("./Combined/4T_2x5/Figures/performance_eval.pdf")
+plt.show()
 
 #print("Ensemble count:", count)
 #print(bf[:,-1])
