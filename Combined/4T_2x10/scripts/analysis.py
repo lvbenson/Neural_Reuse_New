fb6ae543f9fb12ab699b30c5d94d3653c221b555
Benson <lvbenson@iu.edu>
2020-11-05 09:44:24 -0500

    2x10 analysis

--- Combined/4T_2x10/scripts/analysis.py
+++ Combined/4T_2x10/scripts/analysis.py
@@ -577,8 +577,8 @@ for i in range(start,finish):
         # plt.show()
         
         find_all_lesions(dir,i)
-        find_all_var(dir,i)
-        find_all_mis(dir,i)
+        #find_all_var(dir,i)
+        #find_all_mis(dir,i)
         
     index += 1
     #plt.xticks(np.arange(1, 11))
